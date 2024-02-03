c815c7556e7dc7fa3650d99a2cf68d13a07127f4
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-03 10:09:53 +0530

    template added

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
+
+setup(
+    name='Predictive_Maintainance',
+    version='0.0.1',
+    author='Prajwal Krishna',
+    author_email='prajwalgbdr03@gmail.com',
+    #install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
+)
