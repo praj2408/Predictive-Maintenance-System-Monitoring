da477a58a933f6d7f350be108708ba5afee07625
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-09 12:32:36 +0530

    data transformation stage completed

--- src/Predictive_Maintainance/components/data_transformation.py
+++ src/Predictive_Maintainance/components/data_transformation.py
@@ -42,17 +42,25 @@ class DataTransformation:
             logging.info(f"df dataframe head: \n {df.head().to_string()}")
         
             # create type of failure column
-            df.apply(lambda row: type_of_failure(row.name,df), axis=1)
-            df['type_of_failure'].replace(np.NaN, 'no failure', inplace=True)
+            logging.info("create 'type of failure' column")
+            df.apply(lambda row: type_of_failure(row.name, df), axis=1)
+            logging.info("create 'type of failure' column completed successfully")
+            #df['type_of_failure'].replace(np.NaN, 'no failure', inplace=True)
             
             #drop productid and uid
+            
             df.drop(['UDI', 'Product ID'], axis=1, inplace=True)
+            logging.info('removed productid and uid from the dataset')
 
             
             #convert kalvin to celcius
+            logging.info('converting kelvin to celcius')
             df['Air temperature [c]'] = df['Air temperature [K]'] - 273.15
             df['Process temperature [c]'] = df['Process temperature [K]'] - 273.15
             df.drop(['Air temperature [K]', 'Process temperature [K]'], axis=1, inplace=True)
+            
+            logging.info('converted kelvin to celcius successfully')
+
             
             
             # categorical Encoding
@@ -61,20 +69,40 @@ class DataTransformation:
             
             # lable encoding
             df['type_of_failure'] = label_encoder.fit_transform(df['type_of_failure'])
+            
+            logging.info('Encoding completed successfully')
 
             # feature scaling
-            scaled_data = scaler.fit_transform(df)
+            scale_cols = ['Rotational speed [rpm]', 'Torque [Nm]', 'Tool wear [min]', 'Air temperature [c]', 'Process temperature [c]']
             
-            scaled_data_df = pd.DataFrame(scaled_data, columns=df.columns)
+            df_scaled = scaler.fit_transform(df[scale_cols])
+            df_scaled = pd.DataFrame(df_scaled)
+            df_scaled.columns = scale_cols
 
+            df.drop(scale_cols, axis=1, inplace=True)
 
+            df_scaled = pd.concat([df, df_scaled], axis=1)
+            
+            logging.info("data scaling stage completed successfully")
+            
+        
             # oversampling
+            logging.info("Oversampling stage begin.............")
+            smote = SMOTE(sampling_strategy='auto')
             
-    
-            scaled_data_df.to_csv(self.data_transformation_config.preprocessor_csv_file_path, index=False)
+            X = df_scaled.drop('type_of_failure', axis=1)
+            y = df_scaled['type_of_failure']
+
+            X_resampled, y_resampled = smote.fit_resample(X, y)
+
+            df_sampled = pd.concat([X_resampled, y_resampled], axis=1)
+            
+            logging.info("Over sampling stage completed successfully")
+                
+            df_sampled.to_csv(self.data_transformation_config.preprocessor_csv_file_path, index=False)
             
             
-            return scaled_data_df
+            return df_sampled
         
         except Exception as e:
             logging.info("Error occured during DataTransformation stage")
