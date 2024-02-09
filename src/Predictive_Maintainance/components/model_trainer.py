cc6733e6e6e14785172b4e9f0033e87c9ccccd57
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-09 18:46:02 +0530

    update

--- src/Predictive_Maintainance/components/model_trainer.py
+++ src/Predictive_Maintainance/components/model_trainer.py
@@ -4,8 +4,19 @@ import pandas as pd
 import numpy as np
 
 from dataclasses import dataclass
+
+
 from src.Predictive_Maintainance.exception import CustomException
 from src.Predictive_Maintainance.logger import logging
+
+import mlflow
+import mlflow.sklearn
+from evidently import ColumnMapping
+from evidently.test_suite import TestSuite
+from evidently.test_preset import DataStabilityTestPreset
+
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset, DataQualityPreset, ClassificationPreset
 
 
 from sklearn.model_selection import train_test_split
@@ -16,6 +27,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, classification_report
 
 
+lr = LogisticRegression()
+svc = SVC()
+dt = DecisionTreeClassifier()
+rf = RandomForestClassifier()
+
+models = [lr, svc, dt, rf]
+model_name = ["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
 
 
 
@@ -26,12 +44,57 @@ class ModelTrainerConfig:
     trained_model_file_path_2: str = os.path.join("artifacts", "model_2.pkl") # type of failure
     
     
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
     def initiate_model_training(self, train_array, test_array):
         try:
-            pass
+            
+            def model1(df:pd.DataFrame):
+                X = df.drop(["Machine failure", "type_of_failure"], axis=1)
+                y = df["Machine failure"]
+                
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            
+                scores = []
+                for i, m in enumerate(models):
+                    m.fit(X_train.values, y_train.values)
+                    y_pred = m.predict(X_test)
+                    acc = accuracy_score(y_test, y_pred) * 100
+                    prec = precision_score(y_test, y_pred) * 100
+                    rec = recall_score(y_test, y_pred) * 100
+                    f1 = f1_score(y_test, y_pred) * 100
+                    scores.append([acc, prec, rec, f1])
+                
+                with mlflow.start_run():
+                    mlflow.sklearn.log_model(m, model_name[i])
+                    mlflow.log_metric("Accuracy", acc)
+                    mlflow.log_metric("Precision", prec)
+                    mlflow.log_metric("Recall", rec)
+                    mlflow.log_metric("F1", f1)
+                    
+                scores_df = pd.DataFrame(
+                columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
+                                        )
+                scores_df = pd.concat(
+                    [scores_df, pd.DataFrame(scores, columns=["Accuracy", "Precision", "Recall", "F1"])], axis=1
+                                    )
+                
+                best_model_idx = scores_df["F1"].idxmax()
+                best_model_name = scores_df.loc[best_model_idx, 'Model']
+                best_model = models[best_model_idx]
+
+                y_pred = best_model.predict(X_test)
+                report = classification_report(y_test, y_pred, output_dict=True)
+                report = pd.DataFrame(report).transpose()
+
+                logging.info('MODEL 1')
+                logging.info("Best Model: {} ".format(best_model))
+                logging.info(f"Classification Report:\n{report}")
+                
+                return scores_df, best_model, best_model_name, report
+            
         except Exception as e:
             raise CustomException(e, sys)
