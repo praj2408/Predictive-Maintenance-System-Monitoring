664d8a887f5ab5324ada09c57b37357d400b796e
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-02-13 10:35:03 +0530

    mlflow added

--- src/Predictive_Maintainance/components/model_trainer.py
+++ src/Predictive_Maintainance/components/model_trainer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import pickle
 
 from dataclasses import dataclass
 
@@ -33,13 +34,13 @@ dt = DecisionTreeClassifier()
 rf = RandomForestClassifier()
 
 models = [lr, svc, dt, rf]
-#model_name = ["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
+model_name = ["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
 
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path_1: str = os.path.join("artifacts", "model_1.pkl") # binary classification
-    trained_model_file_path_2: str = os.path.join("artifacts", "model_2.pkl") # type of failure
+    model_path_1: str = os.path.join("artifacts", "model1", "model_1.pkl") # for machine failure
+    model_path_2: str = os.path.join("artifacts", "model2", "model_2.pkl") # type of failure
     
     
 class ModelTrainer:
@@ -48,70 +49,109 @@ class ModelTrainer:
         
         
     def Model1(self, df):
+        
         X = df.drop(["Machine failure", "type_of_failure"], axis=1)
         y = df["Machine failure"]
         
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        scores1 = []
         
-        lr.fit(X_train, y_train)
-        y_pred = lr.predict(X_test)
-        acc = accuracy_score(y_test, y_pred) * 100
-        prec = precision_score(y_test, y_pred) * 100
-        rec = recall_score(y_test, y_pred) * 100
-        f1 = f1_score(y_test, y_pred) * 100
-        return [acc, prec, rec, f1]
-    
-    def Model2(self, df):
-        X = df.drop(["Machine failure", "type_of_failure"], axis=1)
-        y = df["type_of_failure"]
+        for i, m in enumerate(models):
+            m.fit(X_train, y_train)
+            y_pred = m.predict(X_test)
+            acc = accuracy_score(y_test, y_pred) * 100
+            prec = precision_score(y_test, y_pred) * 100
+            rec = recall_score(y_test, y_pred) * 100
+            f1 = f1_score(y_test, y_pred) * 100
+            scores1.append([acc, prec, rec, f1])
+            
+            with mlflow.start_run():
+                mlflow.sklearn.log_model(m, model_name[i])
+                mlflow.log_metric("Accuracy", acc)
+                mlflow.log_metric("Precision", prec)
+                mlflow.log_metric("Recall", rec)
+                mlflow.log_metric("F1", f1)
+                
+            
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        scores_df = pd.DataFrame(columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"])
+        scores_df = pd.concat([scores_df, pd.DataFrame(scores1, columns=["Accuracy", "Precision", "Recall", "F1"])], axis=1)
         
-        svc.fit(X_train, y_train)
-        y_pred = svc.predict(X_test)
-        acc = accuracy_score(y_test, y_pred) * 100
-        prec = precision_score(y_test, y_pred) * 100
-        rec = recall_score(y_test, y_pred) * 100
-        f1 = f1_score(y_test, y_pred) * 100
-        return [acc, prec, rec, f1]
+        best_model_idx = scores_df["F1"].idxmax()
+        best_model_name = scores_df.loc[best_model_idx, 'Model']
+        best_model = models[best_model_idx]
 
+        y_pred = best_model.predict(X_test)
+        report = classification_report(y_test, y_pred, output_dict=True)
+        report = pd.DataFrame(report).transpose()
+        
+        logging.info('MODEL 1')
+        logging.info("Best Model: {} ".format(best_model))
+        logging.info(f"Classification Report:\n{report}")
+        
+        
+        return scores_df, best_model, best_model_name, report
+
+
+    def Model2(self, df):
+            
+            X = df.drop(["Machine failure", "type_of_failure"], axis=1)
+            y = df["type_of_failure"]
+            
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            scores1 = []
+            
+            for i, m in enumerate(models):
+                m.fit(X_train, y_train)
+                y_pred = m.predict(X_test)
+                acc = accuracy_score(y_test, y_pred) * 100
+                prec = precision_score(y_test, y_pred,average='macro') * 100
+                rec = recall_score(y_test, y_pred,average='macro') * 100
+                f1 = f1_score(y_test, y_pred,average='macro') * 100
+                scores1.append([acc, prec, rec, f1])
+                
+                with mlflow.start_run():
+                    mlflow.sklearn.log_model(m, model_name[i])
+                    mlflow.log_metric("Accuracy", acc)
+                    mlflow.log_metric("Precision", prec)
+                    mlflow.log_metric("Recall", rec)
+                    mlflow.log_metric("F1", f1)
+                    
+                logging.info(f'model {i} logged to mlflow')
+                
+            
+            scores_df = pd.DataFrame(columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"])
+            scores_df = pd.concat([scores_df, pd.DataFrame(scores1, columns=["Accuracy", "Precision", "Recall", "F1"])], axis=1)
+            
+            best_model_idx = scores_df["F1"].idxmax()
+            best_model_name = scores_df.loc[best_model_idx, 'Model']
+            best_model = models[best_model_idx]
+
+            y_pred = best_model.predict(X_test)
+            report = classification_report(y_test, y_pred, output_dict=True)
+            report = pd.DataFrame(report).transpose()
+            
+            logging.info('MODEL 2: ')
+            logging.info("Best Model: {}".format(best_model))
+            logging.info(f"Classification Report:\n{report}")
+            
+            return scores_df, best_model, best_model_name, report
+    
+    
+    
     def initiate_model_training(self,df):
         try:
             
-                X = df.drop(["Machine failure", "type_of_failure"], axis=1)
-                y = df["Machine failure"]
-                
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            _,model1,_,_= self.Model1(df)
             
-                scores = []
+            with open(self.model_trainer_config.model_path_1, 'wb') as f:
+                pickle.dump(model1, f)
                 
-                for i, m in enumerate(models):
-                    m.fit(X_train, y_train)
-                    y_pred = m.predict(X_test)
-                    acc = accuracy_score(y_test, y_pred) * 100
-                    prec = precision_score(y_test, y_pred) * 100
-                    rec = recall_score(y_test, y_pred) * 100
-                    f1 = f1_score(y_test, y_pred) * 100
-                    scores.append([acc, prec, rec, f1])
-
                 
-                scores_df = pd.DataFrame(columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"])
-                scores_df = pd.concat([scores_df, pd.DataFrame(scores, columns=["Accuracy", "Precision", "Recall", "F1"])], axis=1)
-                
-                best_model_idx = scores_df["F1"].idxmax()
-                best_model_name = scores_df.loc[best_model_idx, 'Model']
-                best_model = models[best_model_idx]
-
-                y_pred = best_model.predict(X_test)
-                report = classification_report(y_test, y_pred, output_dict=True)
-                report = pd.DataFrame(report).transpose()
-
-                logging.info('MODEL 1')
-                logging.info("Best Model: {} ".format(best_model))
-                logging.info(f"Classification Report:\n{report}")
-                
-                print(scores_df, best_model)
-                return scores_df, best_model, best_model_name, report
+            _,model2,_,_= self.Model2(df)
+            
+            with open(self.model_trainer_config.model_path_2, 'wb') as f:
+                pickle.dump(model2, f)
                 
             
         except Exception as e:
--- src/Predictive_Maintainance/pipelines/training_pipeline.py
+++ src/Predictive_Maintainance/pipelines/training_pipeline.py
@@ -3,6 +3,8 @@
 from src.Predictive_Maintainance.components.data_ingestion import DataIngestion
 from src.Predictive_Maintainance.components.data_transformation import DataTransformation
 from src.Predictive_Maintainance.components.model_trainer import ModelTrainer
+# from src.Predictive_Maintainance.components.model_evaluation import ModelEvaluation
+
 
 
 
@@ -25,3 +27,4 @@ df_sampled = data_trainformation.initiate_data_transformation(raw_data_path)
  
 model_trainer = ModelTrainer()
 model_trainer.initiate_model_training(df_sampled)
+
